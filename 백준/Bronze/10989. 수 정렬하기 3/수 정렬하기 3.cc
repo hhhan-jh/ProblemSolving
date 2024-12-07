9a1028efd7d7a048acf8ab17b2d9797ce5248bd8
한정희 <81312141+hhhan-jh@users.noreply.github.com>
2024-12-07 16:56:53 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1540 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -2,6 +2,9 @@
 #include <vector>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
     int n;
     cin>>n;
     vector<int> v(10001);
