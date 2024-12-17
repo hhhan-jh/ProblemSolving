f605d103395d151083c6ff75f17fb1d035177d27
한정희 <81312141+hhhan-jh@users.noreply.github.com>
2024-12-17 21:29:17 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.cc
@@ -0,0 +1,16 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	char ch1, ch2;
+
+	cin >> a >> ch1 >> b >> ch2 >> c;
+
+	if (a + b == c) cout << "YES";
+	else cout << "NO";
+
+    return 0;
+}
